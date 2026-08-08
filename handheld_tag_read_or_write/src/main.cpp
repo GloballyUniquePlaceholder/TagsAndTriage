13871a4e314d2ade53a1aae7f527781fecf304e3
GloballyUniquePlaceholder <265477880+GloballyUniquePlaceholder@users.noreply.github.com>
2026-08-08 19:06:45 +1000

    partial upload

--- handheld_tag_read_or_write/src/main.cpp
+++ handheld_tag_read_or_write/src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h> // https://github.com/arduino-libraries/LiquidCrystal
-// #include <WiFi.h>
+#include <WiFi.h>
 #include <MFRC522.h> // https://github.com/miguelbalboa/rfid
+#include <HTTPClient.h>
+
+
 #define BUTTON_BACK 15
 #define BUTTON_YES  16
 #define BUTTON_NO   4
@@ -24,6 +27,8 @@
 // SCK: 18
 // SS: 5
 
+#define WIFI_SSID "StarLonks"
+#define WIFI_PASS "Slowfi69420"
 
 
 LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
@@ -31,6 +36,7 @@ MFRC522 mfrc522(SS_PIN,RST_PIN);
 
 typedef enum {
   WAITING,
+  IS_MOBILE,
   IS_BREATHING,
   IS_BREATHING_SECONDARY_MESSAGE,
   IS_BREATHING_SECONDARY_CHECK,
@@ -90,7 +96,23 @@ void setup() {
   pinMode(BUTTON_NO, INPUT_PULLUP);
 
   Serial.begin(9600);
-  // WiFi.begin("", "");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID,WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+  }
+  Serial.println(WiFi.gatewayIP());
+  // int n = WiFi.scanNetworks();
+  // for (int i = 0; i < n; ++i) {
+  //     // Print SSID and RSSI for each network found
+  //     Serial.print(i + 1);
+  //     Serial.print(": ");
+  //     Serial.print(WiFi.SSID(i));
+  //     Serial.print(" (");
+  //     Serial.print(WiFi.RSSI(i));
+  //     Serial.print(")");
+  //     Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+  //     delay(10);
+  //   }
   delay(4);
   SPI.begin();
   mfrc522.PCD_Init();
@@ -99,12 +121,65 @@ void setup() {
   lcd.begin(LCD_COLUMNS, LCD_ROWS);
 }
 
+
+typedef enum {
+  WATING,
+  CAN_WALK,
+  BREATHING_GTR_30,
+  BREATHING_AFTER_NOT,
+  NO_RADIAL_PULSE,
+  RESPONSIVE,
+  NOT_RESPONSIVE
+} patient_status_t;
+
+void SaveAndUploadTag(patient_status_t status){
+  WiFiClient client;
+  HTTPClient http;
+  
+  char url[] = "http://192.168.137.1:5001/api/checkin";
+  http.begin(url);
+  http.addHeader("Content-Type","application/json");
+  char patientID[] = "S123";
+  char buffer[256];
+  // this is terrible, this might be a crime 
+  switch (status)
+  {
+  case CAN_WALK:
+    sprintf(buffer, "{\"patient_id\":\"%s\",\"can_walk\":1,\"initial_breathing\":1,\"breathing_after_reposition\":null,\"breathing_rate\":null,\"pulse_present\":null,\"responsive\":null,\"notes\":null}",patientID);
+    break;
+  case BREATHING_AFTER_NOT:
+    sprintf(buffer, "{\"patient_id\":\"%s\",\"can_walk\":0,\"initial_breathing\":0,\"breathing_after_reposition\":1,\"breathing_rate\":null,\"pulse_present\":null,\"responsive\":null,\"notes\":null}",patientID);
+    break;
+  case BREATHING_GTR_30:
+    sprintf(buffer, "{\"patient_id\":\"%s\",\"can_walk\":0,\"initial_breathing\":1,\"breathing_after_reposition\":null,\"breathing_rate\":31,\"pulse_present\":null,\"responsive\":null,\"notes\":null}",patientID);
+    break;
+  case NO_RADIAL_PULSE:
+    sprintf(buffer, "{\"patient_id\":\"%s\",\"can_walk\":0,\"initial_breathing\":1,\"breathing_after_reposition\":null,\"breathing_rate\":null,\"pulse_present\":0,\"responsive\":null,\"notes\":null}",patientID);
+    break;
+    break;
+  case RESPONSIVE:
+    sprintf(buffer, "{\"patient_id\":\"%s\",\"can_walk\":0,\"initial_breathing\":1,\"breathing_after_reposition\":null,\"breathing_rate\":null,\"pulse_present\":1,\"responsive\":1,\"notes\":null}",patientID);
+    break;
+  case NOT_RESPONSIVE:
+    sprintf(buffer, "{\"patient_id\":\"%s\",\"can_walk\":0,\"initial_breathing\":1,\"breathing_after_reposition\":null,\"breathing_rate\":null,\"pulse_present\":1,\"responsive\":0,\"notes\":null}",patientID);
+    break;
+  
+  default:
+    break;
+  }
+  http.POST(buffer);
+  // http.POST();
+  // http.handleHeaderRespons
+}
+
+
 void loop() {
   updateButtons();
 
   switch (appState) {
     case WAITING:
       if (writeOnce) {
+        // SaveAndUploadTag(CAN_WALK);
         writeOnce = 0;
         lcd.clear();
         lcd.setCursor(0, 0);
@@ -119,6 +194,31 @@ void loop() {
       }
       break;
 
+    case IS_MOBILE:
+      if (writeOnce) {
+        writeOnce = 0;
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Can the patient");
+        lcd.setCursor(0, 1);
+        lcd.print("walk?");
+      }
+
+      if (wasPressed(BTN_BACK)) {
+        appState = WAITING;
+        writeOnce = 1;
+      } 
+      if (wasPressed(BTN_NO)) {
+        appState = IS_BREATHING;
+        writeOnce = 1;
+      } 
+      if (wasPressed(BTN_YES)) {
+        // appState = IS_BREATHING_RATE_OVER_30_MIN;
+        //green
+        SaveAndUploadTag(CAN_WALK);
+        writeOnce = 1;
+      }
+      break;
     case IS_BREATHING:
       if (writeOnce) {
         writeOnce = 0;
@@ -130,7 +230,7 @@ void loop() {
       }
 
       if (wasPressed(BTN_BACK)) {
-        appState = WAITING;
+        appState = IS_MOBILE;
         writeOnce = 1;
       } 
       if (wasPressed(BTN_NO)) {
@@ -163,6 +263,7 @@ void loop() {
       }
       if (wasPressed(BTN_YES)) {
         // RED
+        SaveAndUploadTag(BREATHING_GTR_30);
         writeOnce = 1;
       }
       break;
@@ -200,10 +301,12 @@ void loop() {
       }
       if (wasPressed(BTN_YES)) {
         // red
+        SaveAndUploadTag(BREATHING_AFTER_NOT);
         writeOnce = 1;
       }
       if (wasPressed(BTN_NO)) {
         // dead
+        appState = WAITING;
         writeOnce = 1;
       }
       break;
@@ -224,6 +327,7 @@ void loop() {
       }
       if (wasPressed(BTN_NO)) {
         //red
+        SaveAndUploadTag(NO_RADIAL_PULSE);
         writeOnce = 1;
       }
       if (wasPressed(BTN_YES)) {
@@ -247,11 +351,11 @@ void loop() {
         writeOnce = 1;
       }
       if (wasPressed(BTN_NO)) {
-        // red
+        SaveAndUploadTag(NOT_RESPONSIVE);
         writeOnce = 1;
       }
       if (wasPressed(BTN_YES)) {
-        // yellow
+        SaveAndUploadTag(RESPONSIVE);
         writeOnce = 1;
       }
       break;
